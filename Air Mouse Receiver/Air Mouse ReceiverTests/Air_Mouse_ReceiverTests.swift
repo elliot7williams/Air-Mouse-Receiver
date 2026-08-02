fd351a95f81b23e62dfdf29dd7f4e40d42c53a9e
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:34:16 -0400

    Add Air Mouse Receiver/Air Mouse ReceiverTests/Air_Mouse_ReceiverTests.swift

---
+++ Air Mouse Receiver/Air Mouse ReceiverTests/Air_Mouse_ReceiverTests.swift
@@ -0,0 +1,17 @@
+//
+//  Air_Mouse_ReceiverTests.swift
+//  Air Mouse ReceiverTests
+//
+//  Created by Elliot Williams on 2025-07-14.
+//
+
+import Testing
+@testable import Air_Mouse_Receiver
+
+struct Air_Mouse_ReceiverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
